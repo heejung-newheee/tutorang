c2b47f7e8ca812ddcd64f2678321de2e15ebc46e
seonik <ikik.pd@gmail.com>
2023-08-17 12:02:30 +0900

    Hotfix. 파일 구조화 완료

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
-export { default as header } from './common/header/Header';
+export { default as Header } from './common/header/Header';
+export { default as Footer } from './common/footer/Footer';
+export { default as Layout } from './common/layout/Layout';
