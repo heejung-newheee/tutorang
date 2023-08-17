0f4fb57a3b225acb37a4c1c5b2caba040e983c3b
seonik <ikik.pd@gmail.com>
2023-08-18 00:19:06 +0900

    Feat. 공용 모달 body 스크롤 방지 추가

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,5 @@
 import * as S from './Modal.styled';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import ModalPortal from './ModalPortal';
 
 export type ModalProps = {
@@ -9,10 +9,16 @@ export type ModalProps = {
 };
 
 const Modal = ({ children, isOpen, closeModal }: ModalProps) => {
+  // 모달 오픈 시 스크롤 방지
+  useEffect(() => {
+    isOpen ? (document.body.style.overflow = 'hidden') : (document.body.style.overflow = 'auto');
+  }, [isOpen]);
+
   const handleClose = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation();
     closeModal();
   };
+
   return <ModalPortal>{isOpen ? <S.Overlay onClick={handleClose}>{children}</S.Overlay> : null}</ModalPortal>;
 };
 
