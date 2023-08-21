111b7e8da6bec48aac4cc8a0308287ba9ac83c55
heejung-newheee <aldls2311@gmail.com>
2023-08-21 11:40:22 +0900

    Feat: 글로벌 스타일

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -3,17 +3,20 @@ import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Main, Detail, Mypage, SignIn, SignUp } from '../pages';
 import { Layout } from '../components';
+import GlobalLayout from '../components/common/globalLayout/GlobalLayout';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route element={<Layout />}>
-          <Route path="/detail" element={<Detail />} />
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
+        <Route element={<GlobalLayout />}>
+          <Route path="/" element={<Main />} />
+          <Route element={<Layout />}>
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
