6a9de2a2acfe11af2bf1773fc46abd98fe479087
JellyBear97 <olxloha@gamil.com>
2023-08-22 21:42:00 +0900

    Feat: 소셜 회원가입 최초로그인시 추가사용자정보 입력요청
    - 아직 이메일 회원가입은 기능구현 덜함
    - 소셜 회원가입 추가사용자정보 입력 요청 코드도 수정할거 많음

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from '../components';
 import GlobalLayout from '../components/common/globalLayout/GlobalLayout';
-import { Detail, Main, Mypage, SignInForm, SignUpForm, List } from '../pages';
+import { Detail, List, Main, Mypage, SignInForm, SignUpForm } from '../pages';
 import AuthMain from '../pages/AuthMain';
 
 const Router = () => {
@@ -13,10 +13,10 @@ const Router = () => {
           <Route element={<Layout />}>
             <Route path="/detail/:id" element={<Detail />} />
             <Route path="/mypage" element={<Mypage />} />
+            <Route path="/list" element={<List />} />
             <Route element={<AuthMain />}>
               <Route path="/signin" element={<SignInForm />} />
               <Route path="/signup" element={<SignUpForm />} />
-              <Route path="/list" element={<List />} />
             </Route>
           </Route>
         </Route>
