c5023f2162702bea2bf0a99a74e5b314593ee558
JellyBear97 <olxloha@gamil.com>
2023-08-31 18:50:32 +0900

    Chore: git pull from dev

--- src/components/Form/FormHeader.tsx
+++ src/components/Form/FormHeader.tsx
@@ -1,31 +1,38 @@
-import { styled } from 'styled-components';
+// import { useEffect, useState } from 'react';
+// import { styled } from 'styled-components';
 
-const FormHeader = () => {
-  return (
-    <SHeader>
-      <h1>로그인</h1>
-      <p>쉽고 빠르게 튜터를 만나보는 1:1 매칭 서비스 튜터랑</p>
-    </SHeader>
-  );
-};
+// type TypeFormHeaderProps = {
+//   $keyword: string;
+// };
 
-export default FormHeader;
+// const FormHeader: React.FC<TypeFormHeaderProps> = ({ $keyword }) => {
+//   const [formHeaderTitle, setFormHeaderTitle] = useState();
+//   useEffect(() => {}, []);
+//   return (
+//     <SHeader>
+//       <h1>{}</h1>
+//       <p>쉽고 빠르게 튜터를 만나보는 1:1 매칭 서비스 튜터랑</p>
+//     </SHeader>
+//   );
+// };
 
-const SHeader = styled.header`
-  width: 100%;
-  height: 175px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
-  gap: 10px;
-  & h1 {
-    font-size: 32px;
-    font-weight: 700;
-  }
-  & p {
-    font-size: 20px;
-    color: #4a4a4a;
-  }
-`;
+// export default FormHeader;
+
+// const SHeader = styled.header`
+//   width: 100%;
+//   height: 175px;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 20px;
+//   gap: 10px;
+//   & h1 {
+//     font-size: 32px;
+//     font-weight: 700;
+//   }
+//   & p {
+//     font-size: 20px;
+//     color: #4a4a4a;
+//   }
+// `;
