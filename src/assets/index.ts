a3ddf66cc1af436cccfbee6effa1dcdb0b958596
heejung-newheee <aldls2311@gmail.com>
2023-09-02 19:32:52 +0900

    Feat:footer

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -23,6 +23,7 @@ export { default as bubble } from './talk_bubble.png';
 export { default as tabPanel_1 } from './tab-panel-1.png';
 export { default as tabPanel_3 } from './tab-panel-3.png';
 export { default as main_banner } from './main-banner.png';
+export { default as foot_logo } from './foot_logo.png';
 export { default as icon_more } from './icon_dot_edit.svg';
 export { default as icon_bookMark_full } from './icon_bookMark_full.svg';
 export { default as icon_bookMark_empty } from './icon_bookMark.svg';
--- src/components/common/footer/Footer.syled.ts
+++ src/components/common/footer/Footer.syled.ts
@@ -2,8 +2,42 @@ import { styled } from 'styled-components';
 
 export const FooterContainer = styled.footer`
   background-color: #f9f9f9;
+  margin-top: 100px;
 `;
 export const Inner = styled.div`
   max-width: 1200px;
   margin: 0 auto;
+`;
+export const FooterWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 14px;
+  color: #444;
+  > div {
+    width: 25%;
+    &:first-child {
+      width: 50%;
+    }
+  }
+`;
+export const CompanyInfo = styled.div`
+  display: flex;
+  gap: 30px;
+  margin-bottom: 50px;
+`;
+
+export const Call = styled.div`
+  font-size: 22px;
+  font-weight: bold;
+  line-height: 1;
+`;
+
+export const FootLink = styled.div`
+  p {
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+  li {
+    line-height: 2;
+  }
 `;
