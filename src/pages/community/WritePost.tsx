6a8198c533e63084210de0a1257366e8e9d0441f
Yujin Kim <cocodj0509@naver.com>
2023-09-06 20:54:19 +0900

    Feat: editor추가

--- src/pages/community/WritePost.tsx
+++ src/pages/community/WritePost.tsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useMemo, useRef, useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import supabase from '../../supabase';
+import './write.css';
 
 const WritePost = () => {
-  return <div>WritePost</div>;
+  const QuillRef = useRef<ReactQuill>();
+  const [contents, setContents] = useState('');
+
+  const imageHandler = () => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = async () => {
+      if (input.files) {
+        const file = input.files[0];
+        console.log(file);
+
+        try {
+          const { data, error } = await supabase.storage.from('test222').upload('test222/' + file.name, file, {
+            cacheControl: '3600',
+            upsert: false,
+          });
+
+          // const { data: img } = supabase.storage
+          //   .from("test222")
+          //   .getPublicUrl(file.name);
+
+          // console.log(img);
+          // if (data) {
+          //   const fileURL = data.Location;
+          // }
+
+          console.log(data, error);
+        } catch {}
+      }
+    };
+  };
+
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [['bold', 'italic', 'underline', 'strike', 'blockquote'], [{ color: [] }], [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }, { align: [] }], ['image']],
+        handlers: {
+          image: imageHandler,
+        },
+      },
+    }),
+    [],
+  );
+
+  return (
+    <div style={{ margin: 100 }}>
+      <div>title</div>
+      <ReactQuill
+        ref={(element) => {
+          if (element !== null) {
+            QuillRef.current = element;
+          }
+        }}
+        value={contents}
+        onChange={setContents}
+        modules={modules}
+        className="quill"
+        theme="snow"
+        placeholder="내용을 입력해주세요."
+      />
+    </div>
+  );
 };
 
 export default WritePost;
