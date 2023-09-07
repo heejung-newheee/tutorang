6fb90cd61f1f05c90068f96db6e1b5a196b4fd53
JellyBear97 <olxloha@gamil.com>
2023-09-08 00:28:17 +0900

    Fix : 소셜 로그인시 header에 이름, role 불러오기
    - app.tsx에서 redux로 로그인 사용자 정보 넣기 전에 추가 알고리즘 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState, useCallback } from 'react';
+import { Session } from '@supabase/supabase-js';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { getUser } from './api/user';
+import { getUser, userUpdateAndGet } from './api/user';
 import { Loading } from './components';
 import { logOutUser, setUser } from './redux/modules/user';
 import Router from './shared/Router';
 import GlobalStyle from './style/GlobalStyle';
 import supabase from './supabase';
-import { Session } from '@supabase/supabase-js';
 
 function App() {
   const dispatch = useDispatch();
@@ -15,11 +15,27 @@ function App() {
   const handleAuth = useCallback(
     (session: Session | null) => {
       if (session) {
-        getUser(session.user.email).then((data) => {
-          if (data) dispatch(setUser(data));
-          else dispatch(logOutUser());
-          setLoading(false);
-        });
+        console.log('이건 session', session);
+        getUser(session.user.email)
+          .then((data) => {
+            console.log('이것은 내가 원하는 것이야 db profile data', data);
+            if (data!.username === null) {
+              const userName = session.user.user_metadata.name;
+              const additionalData = {
+                username: userName,
+                role: 'student',
+              };
+              const renewalData = userUpdateAndGet(additionalData, session.user.id);
+              return renewalData;
+            } else {
+              return data;
+            }
+          })
+          .then((data) => {
+            if (data) dispatch(setUser(data));
+            else dispatch(logOutUser());
+            setLoading(false);
+          });
       } else {
         dispatch(logOutUser());
         setLoading(false);
