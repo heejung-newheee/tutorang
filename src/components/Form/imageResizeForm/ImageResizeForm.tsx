a686b97af318e5ebc7f26a86ef5af048b5ed7507
heejung-newheee <aldls2311@gmail.com>
2023-09-16 01:07:03 +0900

    Fix : build 오류

--- src/components/Form/imageResizeForm/ImageResizeForm.tsx
+++ src/components/Form/imageResizeForm/ImageResizeForm.tsx
@@ -9,20 +9,6 @@ import { RootState } from '../../../redux/config/configStore';
 import { closeModal, successModal } from '../../../redux/modules';
 import * as S from './ImageResizeForm.styled';
 
-const base64StringtoFile = (base64String: string, filename: string): File => {
-  const arr = base64String.split(',');
-  const mime = arr[0].match(/:(.*?);/)![1];
-  const bstr = atob(arr[1]);
-  let n = bstr.length;
-  const u8arr = new Uint8Array(n);
-
-  while (n--) {
-    u8arr[n] = bstr.charCodeAt(n);
-  }
-
-  return new File([u8arr], filename, { type: mime });
-};
-
 const ImageResizeForm = () => {
   const { targetId } = useSelector((state: RootState) => state.modal);
   const [image, setImage] = useState<string | null>(null);
