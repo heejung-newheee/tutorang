b9070397233462bb9e7ccf0708b2f0b860da1886
Yujin Kim <cocodj0509@naver.com>
2023-09-16 01:53:06 +0900

    pull전 커밋

---
+++ src/@types/PostDetail/PostDetailType.ts
@@ -0,0 +1,5 @@
+export type PostUpdate = {
+  post_id: number | undefined | null;
+  comment: string;
+  user_id: string | null;
+};
--- src/api/writeCommunity.ts
+++ src/api/writeCommunity.ts
@@ -1,12 +1,23 @@
 import supabase from '../supabase';
 
-export const WriteInsertApi = async (newInfo: any) => {
+type Community = {
+  title: string;
+  content: string;
+  user_id?: string;
+  category: string;
+};
+
+type Edit = {
+  title: string;
+  content: string;
+};
+export const WriteInsertApi = async (newInfo: Community) => {
   const { error } = await supabase.from('write').insert(newInfo);
 
   if (error) throw error;
 };
 
-export const editUpdateApi = async (newInfo: any, editPostNum: number) => {
+export const editUpdateApi = async (newInfo: Edit, editPostNum: number) => {
   const { error } = await supabase.from('write').update(newInfo).eq('id', editPostNum);
 
   if (error) throw error;
